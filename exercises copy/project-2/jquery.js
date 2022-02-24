6d95a01a65eb0b8d8a1a7bdc763c643ac7d7a81b
paths314 <paths314@newschool.edu>
2022-02-24 08:57:31 -0500

    project 2 changes

--- exercises copy/project-2/jquery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {alert("[hello hello hello]"); })
-
-const demoDiv = document.querySelector("#demo");
-window.addEventListener('scroll', function() {
-  if (pageYOffset*0.0001 > 1 || pageYOffset*0.0001 < 0.2) { return; }
-  else { demo.setAttribute('style', 'transform: scale('+pageYOffset*0.0001+');'); }
-});
