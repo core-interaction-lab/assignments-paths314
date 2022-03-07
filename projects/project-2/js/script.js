aebe8848753dd1fea1bf9e81d3068db2c989724b
paths314 <paths314@newschool.edu>
2022-03-07 05:31:10 -0500

    project 2 changes

--- projects/project-2/js/script.js
+++ projects/project-2/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
-    $(window).on('scroll',function(){
+  var link = $('.navbar a.dot');
+    $(window).on('scroll',function(evt){
   
-      var link = $('.navbar a.dot');
+        
+
       var top = $(window).scrollTop();
   
       $('.sec').each(function(){
@@ -11,11 +12,23 @@ $(document).ready(function(){
         var offset = $(this).offset().top - 150;
         if (top >= offset && top < offset + height){
           link.removeClass('active');
-          $('.navbar').find('[data-scroll="' + id + '"]') .addClass('active');
+          $('.navbar').find('[data-scroll="' + id + '"]').addClass('active');
         }
       });
   
     });
+
+    link.click(function(evt) {
+console.log(evt);
+link.removeClass('active');
+/*
+link.each(function(index, el) {
+  console.log(el);
+el.removeClass('active');
+})
+*/
+      evt.target.classList.add('active');
+    })
 
     const demoDiv = document.querySelector("#demo");
 window.addEventListener('scroll', function() {
