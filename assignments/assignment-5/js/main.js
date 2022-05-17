da35af6cc45d3bf03a5a6592b35572f549e92b21
paths314 <paths314@newschool.edu>
2022-05-17 14:47:13 -0400

    assignment 5 redo

--- assignments/assignment-5/js/main.js
+++ assignments/assignment-5/js/main.js
@@ -1,61 +1,82 @@
-function fetchPrompt () {
-    var fashionPrompt = document.getElementById('prompt');
-    var prompts = ['Pasta', 'Fried Rice', 'Tomato Soup', 'Sandwich', 'Pizza', 'Milkshake', 'Burrito', 'Burger'];
-    var prompt = prompts[Math.floor(Math.random()*prompts.length)];
-    fashionPrompt.append(prompt);
-}
-fetchPrompt();
+const formEl = document.getElementById('madlib-form');
 
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
 
-const todoTextarea = document.getElementById('todo-textarea');
-const addTodoBtn = document.getElementById('add-todo-btn');
-const todoContainer = document.getElementById('todo-container');
+    const formData = new FormData(formEl);
 
+    console.log(formData.get('input-1'));
+    const spanEls = document.querySelectorAll('.input-value');
 
-const state = {
-    todoItems: [], 
-};
-
-const setTodoItems = items => {
-    state.todoItems = items;
-};
-
-const addTodoItem = item => {
-    const todoItemsCopy = state.todoItems.slice();
-    todoItemsCopy.push(item);
-    setTodoItems(todoItemsCopy);
-};
-
-const removeTodoItem = (index) => {
-    const todoItemsCopy = state.todoItems.slice();
-    todoItemsCopy.splice(index, 1);
-    setTodoItems(todoItemsCopy);
-};
-
-const buildTodoItem = (item, index) => {
-    const todoEl = document.createElement('article');
-    const textEl = document.createElement('p');
-
-    textEl.innerHTML = item;
-
-    todoEl.append(textEl);
-    return todoEl;
-};
-
-const buildTodoItems = items => {
-    todoContainer.innerHTML = '';
-    const todoItemsEls = items.map(buildTodoItem);
-    todoContainer.append(...todoItemsEls);
-};
-
-const main = () => {
-    addTodoBtn.addEventListener('click', evt => {
-        const todoValue = todoTextarea.value;
-        if (todoValue.length > 0) {
-            addTodoItem(todoValue);
-            buildTodoItems(state.todoItems);
-        }
+    spanEls.forEach(span => {
+        span.innerHTML = formData.get(span.dataset.input);
     });
-};
+});
 
-main();
+
+
+
+
+
+
+// function fetchPrompt () {
+//     var fashionPrompt = document.getElementById('prompt');
+//     var prompts = ['Pasta', 'Fried Rice', 'Tomato Soup', 'Sandwich', 'Pizza', 'Milkshake', 'Burrito', 'Burger'];
+//     var prompt = prompts[Math.floor(Math.random()*prompts.length)];
+//     fashionPrompt.append(prompt);
+// }
+// fetchPrompt();
+
+
+// const todoTextarea = document.getElementById('todo-textarea');
+// const addTodoBtn = document.getElementById('add-todo-btn');
+// const todoContainer = document.getElementById('todo-container');
+
+
+// const state = {
+//     todoItems: [], 
+// };
+
+// const setTodoItems = items => {
+//     state.todoItems = items;
+// };
+
+// const addTodoItem = item => {
+//     const todoItemsCopy = state.todoItems.slice();
+//     todoItemsCopy.push(item);
+//     setTodoItems(todoItemsCopy);
+// };
+
+// const removeTodoItem = (index) => {
+//     const todoItemsCopy = state.todoItems.slice();
+//     todoItemsCopy.splice(index, 1);
+//     setTodoItems(todoItemsCopy);
+// };
+
+// const buildTodoItem = (item, index) => {
+//     const todoEl = document.createElement('article');
+//     const textEl = document.createElement('p');
+
+//     textEl.innerHTML = item;
+
+//     todoEl.append(textEl);
+//     return todoEl;
+// };
+
+// const buildTodoItems = items => {
+//     todoContainer.innerHTML = '';
+//     const todoItemsEls = items.map(buildTodoItem);
+//     todoContainer.append(...todoItemsEls);
+// };
+
+// const main = () => {
+//     addTodoBtn.addEventListener('click', evt => {
+//         const todoValue = todoTextarea.value;
+//         if (todoValue.length > 0) {
+//             addTodoItem(todoValue);
+//             buildTodoItems(state.todoItems);
+//         }
+//     });
+// };
+
+// main();
